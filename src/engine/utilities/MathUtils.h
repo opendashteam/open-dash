9590ccb2035cf3a8b66fccc1bb88bd52eb4e9301
StarryDawn72 <artin.g4000@gmail.com>
2026-09-22 22:00:57 -0400

    fix stuff and add math utils (two of them)

--- src/engine/utilities/MathUtils.h
+++ src/engine/utilities/MathUtils.h
@@ -1,1 +1,16 @@
-// TODO: static math utils (e.g. equivalent of CC_RADIANS_TO_DEGREES, etc.)
+#pragma once
+
+namespace opendash::engine
+{
+
+inline float deg(float radians) {
+    return radians * 57.29577951f; // Match CC_RADIANS_TO_DEGREES
+}
+
+inline float rad(float degrees) {
+    return degrees * 0.01745329252f; // Match CC_DEGREES_TO_RADIANS
+}
+
+// TODO slerp2D, squareDistance, etc...
+    
+}
