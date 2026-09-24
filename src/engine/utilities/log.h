ea6f7f1a950ec398d8ffb745451d8b153d5ffb64
IliasHDZ <11091568+iliasHDZ@users.noreply.github.com>
2026-09-24 15:13:46 +0200

    logger colors

--- src/engine/utilities/log.h
+++ src/engine/utilities/log.h
@@ -7,17 +7,17 @@ void log(const std::string& msg);
 
 template <typename ...Args>
 inline void info(fmt::format_string<Args...> fmtStr, Args&&... args) {
-    log("[INFO] " + fmt::format(fmtStr, std::forward<Args>(args)...));
+    log("\e[0;34m[INFO]\e[0m " + fmt::format(fmtStr, std::forward<Args>(args)...));
 }
 
 template <typename ...Args>
 inline void warn(fmt::format_string<Args...> fmtStr, Args&&... args) {
-    log("[WARN] " + fmt::format(fmtStr, std::forward<Args>(args)...));
+    log("\e[0;33m[WARN]\e[0m " + fmt::format(fmtStr, std::forward<Args>(args)...));
 }
 
 template <typename ...Args>
 inline void err(fmt::format_string<Args...> fmtStr, Args&&... args) {
-    log("[ERR] " + fmt::format(fmtStr, std::forward<Args>(args)...));
+    log("\e[0;31m[ERR] " + fmt::format(fmtStr, std::forward<Args>(args)...) + "\e[0m");
 }
 
 };
